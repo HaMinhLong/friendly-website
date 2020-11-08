1566e8515d6f431fb581d7e1c16c06609fb7ceeb
Minh Long <haminhlong3@gmail.com>
2020-11-08 15:56:54 +0000

    update Menu

--- src/components/content/Menu.js
+++ src/components/content/Menu.js
@@ -1,10 +1,6 @@
 import React from 'react'
-import menu1 from '../../images/menu1.jpg'
-import menu2 from '../../images/menu2.jpg'
-import menu3 from '../../images/menu3.jpg'
-import menu4 from '../../images/menu4.jpg'
-import menu5 from '../../images/menu5.jpg'
-import menu6 from '../../images/menu6.jpg'
+import FoodsDetails from './menuFoods/FoodsDetails'
+
 
 function Menu() {
   return (
@@ -15,63 +11,7 @@ function Menu() {
             60% of businesses are not having a content marketing plan/strategy.
           </p>
         </div>
-
-        <div className='content'>
-          <div className='box'>
-            <div className='imgBx'>
-              <img src={menu1} alt='menu1'/ >
-            </div>
-            <div className='text'>
-              <h3>Special Salads</h3>
-            </div>
-          </div>
-
-          <div className='box'>
-            <div className='imgBx'>
-              <img src={menu2} alt='menu2'/ >
-            </div>
-            <div className='text'>
-              <h3>Special Soup</h3>
-            </div>
-          </div>
-
-          <div className='box'>
-            <div className='imgBx'>
-              <img src={menu3} alt='menu3'/ >
-            </div>
-            <div className='text'>
-              <h3>Special Pasta</h3>
-            </div>
-          </div>
-
-          <div className='box'>
-            <div className='imgBx'>
-              <img src={menu4} alt='menu4'/ >
-            </div>
-            <div className='text'>
-              <h3>Special Salads</h3>
-            </div>
-          </div>
-
-          <div className='box'>
-            <div className='imgBx'>
-              <img src={menu5} alt='menu5'/ >
-            </div>
-            <div className='text'>
-              <h3>Special Soup</h3>
-            </div>
-          </div>
-
-          <div className='box'>
-            <div className='imgBx'>
-              <img src={menu6} alt='menu6'/ >
-            </div>
-            <div className='text'>
-              <h3>Special Pasta</h3>
-            </div>
-          </div>
-        </div>
-        
+        <FoodsDetails />
         <div className='title'>
           <a href="#home" className='btn'>View All</a>
         </div>
