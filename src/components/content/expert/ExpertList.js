364578fcaa856292979f890d573e96cb793b5fe5
Minh Long <haminhlong3@gmail.com>
2020-11-08 16:10:26 +0000

    add expert list

---
+++ src/components/content/expert/ExpertList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ExpertsDetails from './ExpertsDetails'
+
+function ExpertList() {
+  return (
+    <div>
+      <ExpertsDetails />
+    </div>
+  )
+}
+
+export default ExpertList
