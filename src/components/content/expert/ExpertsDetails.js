93f1c28cc130aa21f6355529cd904e1974e978f5
Minh Long <haminhlong3@gmail.com>
2020-11-08 16:10:50 +0000

    add expert details

---
+++ src/components/content/expert/ExpertsDetails.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import expert1 from '../../../images/expert1.jpg'
+import expert2 from '../../../images/expert2.jpg'
+import expert3 from '../../../images/expert3.jpg'
+import expert4 from '../../../images/expert4.jpg'
+
+function ExpertsDetails() {
+  return (
+    <div>
+      <div className='content'>
+        <div className='box'>
+          <div className='imgBx'>
+            <img src={expert1} alt='expert famous'/>
+          </div>
+          <div className='text'>
+            <h3>Some Famous</h3>
+          </div>
+        </div>
+
+        <div className='box'>
+          <div className='imgBx'>
+            <img src={expert2} alt='expert famous' />
+          </div>
+          <div className='text'>
+            <h3>Some Famous</h3>
+          </div>
+        </div>
+
+        <div className='box'>
+          <div className='imgBx'>
+            <img src={expert3} alt='expert famous' />
+          </div>
+          <div className='text'>
+            <h3>Some Famous</h3>
+          </div>
+        </div>
+
+        <div className='box'>
+          <div className='imgBx'>
+            <img src={expert4} alt='expert famous' />
+          </div>
+          <div className='text'>
+            <h3>Some Famous</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ExpertsDetails
