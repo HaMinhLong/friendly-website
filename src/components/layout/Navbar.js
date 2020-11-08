16012553e44c2d6a7d6d85fb8ad821b23aa6787c
Minh Long <haminhlong3@gmail.com>
2020-11-08 14:12:30 +0000

    Add navigation

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Navbar() {
+
+  window.addEventListener('scroll', () => {
+    const header = document.querySelector('header')
+    header.classList.toggle("sticky", window.scrollY > 0)
+  })
+
+  return (
+    <header>
+      <a href='#' className='logo'>Friendly Food <span>.</span></a>
+      <ul className='navigation'>
+        <li><a href='#banner'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#menu'>Menu</a></li>
+        <li><a href='#expert'>Expert</a></li>
+        <li><a href='#testimonials '>Testimonials</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+    </header>
+  )
+}
+
+export default Navbar
