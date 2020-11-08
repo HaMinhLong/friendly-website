ec6c66b98b2e7a86aaea0a0afd5efd22c2b2d74d
Minh Long <haminhlong3@gmail.com>
2020-11-08 14:25:32 +0000

    add banner in layout

---
+++ src/components/layout/Banner.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <section className='banner' id='banner'>
+        <div className='content'>
+          <h2>Always Choose Good</h2>
+          <p>
+          60% of businesses are not having a content marketing plan/strategy. 
+          That means that a majority of business owners are building content 
+          without a clear strategy behind. If you do not have a clear focused 
+          content marketing plan you are in dangerous territory because you 
+          are very likely in the risk of creating content that is not on par 
+          with your brands business values. Creating a solid plan/strategy 
+          is a clear necessity.
+          </p>
+          <a href='#home' className='btn'>Our Menu</a>
+        </div>
+    </section>
+  )
+}
+
+export default Banner
