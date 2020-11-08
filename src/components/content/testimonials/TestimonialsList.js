41c01fe0f452770cd3d29007002dfcbcfae5feb8
Minh Long <haminhlong3@gmail.com>
2020-11-08 16:53:13 +0000

    add testimonials list

---
+++ src/components/content/testimonials/TestimonialsList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TestimonialsDetails from './TestimonialsDetails'
+
+function TestimonialsList() {
+  return (
+    <div>
+      <TestimonialsDetails />
+    </div>
+  )
+}
+
+export default TestimonialsList
