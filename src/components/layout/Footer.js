7466f0ef5372b5c8f4de497f0cd5654ccf092e20
Minh Long <haminhlong3@gmail.com>
2020-11-08 17:21:38 +0000

    add Footer

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <p>Copyright 2020 <a href='#home'>Naruto</a>. All right Reserved</p>
+    </footer>
+  )
+}
+
+export default Footer
